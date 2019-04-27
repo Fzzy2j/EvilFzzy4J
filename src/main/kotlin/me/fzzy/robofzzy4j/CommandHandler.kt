1f850c4b1aefece7a38c4afce26cd32e5097c3bf
Fzzy <fzzysoda@gmail.com>
2019-04-26 18:17:04 -0700

    little update

--- src/main/kotlin/me/fzzy/robofzzy4j/CommandHandler.kt
+++ src/main/kotlin/me/fzzy/robofzzy4j/CommandHandler.kt
@@ -95,7 +95,7 @@ object CommandHandler {
                         }
                         true
                     }
-                }
+                } else tryDelete(event.message)
 
                 msgBuilder.execute()
             }
